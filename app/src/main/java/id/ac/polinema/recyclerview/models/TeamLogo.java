2180ba01603ca4a6de1fef28eb77c8110bcc599b
raize <raizenkimochi12@gmail.com>
2020-03-02 15:52:21 +0700

    SimpleActivity + GridActivity

--- app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
+++ app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
@@ -1,5 +1,25 @@
 package id.ac.polinema.recyclerview.models;
 
 public class TeamLogo {
+    public String logo;
+    public String name;
 
+    public TeamLogo(String s, String liverpool) {
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
